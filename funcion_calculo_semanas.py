a5f65f9fa90704f118c5bc0728c80d97d105c88f
fulkesko <torentk@live.com>
2018-06-04 00:18:13 -0400

    arreglo del problem
    cargar a variable xD

--- funcion_calculo_semanas.py
+++ funcion_calculo_semanas.py
@@ -1,15 +1,32 @@
 from datetime import datetime
+
 def ultima_regla():
 
     print("ingrese la fecha de su ultima regla")
     dia = int(input("dia: "))
     mes = int(input("mes: "))
-    año = int(input("año: "))
+    anio = int(input("año: "))
     fecha1 = datetime.now()
-    fecha2 = datetime(año, mes, dia, 0, 0, 0)
+    fecha2 = datetime(anio, mes, dia, 0, 0, 0)
     diferencia = fecha1 - fecha2
-    diferencia = diferencia / 7
-    return (diferencia)
+    semanas = diferencia / 7
+    return (semanas.days)
 
 
 
+
+datos = {'nom': "", 'semanas': "", 'pelo_papa': "", 'pelo_mama': "", 'tipo_part': ""}
+
+def ingreso_datos():
+    while(True):
+        nombre = input("ingrese su nombre: ")
+        datos['nom'] = nombre
+        t_emba = ultima_regla()
+        print (t_emba,"semanas de embarazo")
+        datos['semanas'] = t_emba
+        pelo_p = input("ingrese color de pelo del papá: ")
+        datos['pelo_papa'] = pelo_p
+        pelo_m = input("ingrese color de pelo de la mamá: ")
+        datos['pelo_mama'] = pelo_m
+        t_part = input("¿Que tipo de parto espera tener?: ")
+        datos['tipo_part'] = t_part
