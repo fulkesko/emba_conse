d7f10227affeded0fa40b2d15877a409c408eb88
Flavio Luciano Toro Basso <ftoro@svr.lab315>
2018-06-05 13:26:42 -0400

    versiones, linux falta gif de momento

--- menu_principal_linux.py
+++ menu_principal_linux.py
@@ -1,7 +1,7 @@
 from funcion_calculo_semanas import *
 
 def menu_p():
-
+    print("                            ")
     print("bienvenida a el programa registros de datos y consejos")
     print("                            ")
     print("por favor seleccione la opcion que desea")
@@ -18,13 +18,8 @@ def menu_p():
         if(opci.lower() == "registrar"):
             menu_dat()
         elif(opci.lower() == "ver"):
-            if (datos['nombre'] == "nada"):
-                gifto()
-                for x in datos:
-                    print(x, ":", datos[x])
-                else:
-                    for x in datos:
-                        print (x,":",datos[x])
+            for x in datos:
+                print(x, ":", datos[x])
         if (opci.lower() == "salir"):
             print("gracias por usar esta app")
             break
